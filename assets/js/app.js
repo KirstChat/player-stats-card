353217fb6de97687a1bbfc83b78a105720128d07
Kirsty Chatterton <chattertonkirsty@gmail.com>
2022-03-05 19:39:47 +0000

    add function that creates list of player names

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+const dropdownBtn = document.querySelector('.dropdown__btn');
+const dropdownList = document.querySelector('.dropdown__list');
+
+// Get data from player-stats.json
 const getData = async () => {
   const res = await fetch('../../data/player-stats.json');
   const data = await res.json();
@@ -5,4 +9,26 @@ const getData = async () => {
   return data;
 };
 
-getData().then((data) => console.log('Players: ', data.players));
+// Create list of player names
+const listPlayers = () => {
+  getData().then(data => {
+    const { players } = data;
+
+    players.forEach((player, i) => {
+      const playerName = player.player.name;
+      console.log(playerName.first, playerName.last);
+
+      const ul = document.createElement('ul');
+      ul.innerHTML = `
+        <li class="box--grey">${playerName.first} ${playerName.last}</li>
+      `;
+      dropdownList.appendChild(ul);
+    });
+  });
+};
+
+listPlayers();
+
+dropdownBtn.addEventListener('click', () => {
+  dropdownList.classList.toggle('hidden');
+});
